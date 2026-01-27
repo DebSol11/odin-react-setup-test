25b27ec2eab6c17bed5846b94ea44b362615ffb9
Michael <100490812+DebSol11@users.noreply.github.com>
2026-01-27 17:28:35 +0400

    Learning React

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+  return (
+    <div>
+      <h1>Favorite Food</h1>
+      <div>
+        <img src="./img/luisa-brimble-2RrBE90w0T8-unsplash.jpg" alt="salads" />
+        <br />
+        <span>
+            <b>Salads</b>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export { Food };
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>I swear nothing.</h1>;
+}
+
+export default Greeting;
