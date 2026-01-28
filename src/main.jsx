1d1114a955c3e9e0440c8abd735e33f2ff73480c
Michael <100490812+DebSol11@users.noreply.github.com>
2026-01-28 18:59:27 +0400

    Update Code

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
 import { Food } from "./Food.jsx";
 import { Test } from "./HTMLtoJSX.jsx";
+import { Animals } from "./Animals.jsx";
+import { AnimalsConditionally } from "./AnimalsConditionally.jsx";
+import { DrinkList } from "./Drink.jsx";
+import { RecipeList } from "./Recipes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <RecipeList />
     <Greeting />
     <Food />
     <Test />
+    <Animals />
+    <AnimalsConditionally />
+    <DrinkList />
   </StrictMode>,
 );
